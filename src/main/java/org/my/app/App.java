612786e3dbfc1603cb721cf18aaa27eb24602cdb
Jeremy Long <jeremy.long@gmail.com>
2021-08-14 07:12:15 -0400

    initial

---
+++ src/main/java/org/my/app/App.java
@@ -0,0 +1,13 @@
+package org.my.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Hello GitHub Actions");
+    }
+}
